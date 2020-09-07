98a9e78d3eaee22a210fec2a9bab329ef8871c94
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2020-09-07 16:06:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,13 +2,20 @@ var bullet,wall;
 
 var speed, thickness, weight;
 
+var start, startIMG;
+
+function preload(){
+  startIMG=loadImage("start.png");
+}
+
 function setup() {
 
   createCanvas(1600,400);
 
-  bullet = createSprite(100, height/2, 10, 10);
-  bullet.velocityX = speed;
+  start=createSprite(400,100,10,10);
+  start.addImage("startButton", startIMG);
 
+  bullet = createSprite(300, height/2, 10, 10)
 
   wall  = createSprite(1200, 200, thickness,height/2);
 
@@ -17,27 +24,31 @@ function setup() {
   thickness = random(22,83);
 }
 
-function draw() {
+function draw(){
   background("black");
 
   bullet.shapeColor=color(255,255,255);
 
   wall.shapeColor=color(80,80,80);
 
-  if(wall.x-bullet.x<(bullet.width+wall.width)/2){
-    bullet.velocityX = 0;
+  if(mousePressedOver(start)){
+    bullet.velocityX = speed;
+  }
 
-    var damage = 0.5 * weight * speed * speed/22509;
+  if(wall.x-bullet.x<(bullet.width+wall.width)/2){
+   bullet.velocityX = 0;
+
+   var damage = 0.5 * weight * speed * speed/22509;
     
     if(damage<10){
       wall.shapeColor=color(green);
-    }
+   }
 
-    if(damage>10){
+   if(damage>10){
       wall.shapeColor=color(red);
     }
 
   }
 
-  drawSprites();
+ drawSprites();
 }
